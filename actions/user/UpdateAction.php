7241a794bf003faa74afc8ca8117660bf13bc92c
Moischik <mike.rorikstead@gmail.com>
2021-05-04 22:24:37 +0300

    Create UpdateAction class for users

---
+++ actions/user/UpdateAction.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace app\actions\user;
+
+
+use app\models\User;
+use Yii;
+use yii\base\Action;
+use yii\helpers\Json;
+use yii\web\NotFoundHttpException;
+
+class UpdateAction extends Action
+{
+    public function run($id)
+    {
+        if ($this->controller->whoAreyou()== false) {
+            $this->controller->redirect('/site/index');
+        }
+
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post())) {
+
+            if ($model->save()) {
+                return $this->controller->redirect(['view', 'id' => $model->id]);
+            } else {
+                Yii::$app->session->setFlash('error', Json::encode($model->getErrors()));
+            }
+        }
+
+
+        return $this->controller->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    protected function findModel($id)
+    {
+        if (($model = User::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+
+}
