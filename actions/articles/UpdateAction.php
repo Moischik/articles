37d7c0ae139a7b39b666984ddc687c4a3b54483f
Moischik <mike.rorikstead@gmail.com>
2021-05-04 23:14:28 +0300

    Mod UpdateAction class for articles fix model->save

--- actions/articles/UpdateAction.php
+++ actions/articles/UpdateAction.php
@@ -7,25 +7,32 @@ namespace app\actions\articles;
 use app\models\Articles;
 use Yii;
 use yii\base\Action;
+use yii\helpers\Json;
 use yii\web\NotFoundHttpException;
 
 class UpdateAction extends Action
 {
     public function run($id)
     {
-        if ($this->controller->whoAreyou()== false) {
+        if ($this->controller->whoAreyou() == false) {
             $this->controller->redirect('/site/index');
         }
         $model = $this->findModel($id);
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->controller->redirect(['view', 'id' => $model->id]);
+        if ($model->load(Yii::$app->request->post())) {
+            if ($model->save()) {
+                return $this->controller->redirect(['view', 'id' => $model->id]);
+            } else {
+                Yii::$app->session->setFlash('error', Json::encode($model->getErrors()));
+            }
         }
 
-        return $this->controller->render('update', [
-            'model' => $model,
-        ]);
-    }
+            return $this->controller->render('update', [
+                'model' => $model,
+            ]);
+        }
+
+
     protected function findModel($id)
     {
         if (($model = Articles::findOne($id)) !== null) {
