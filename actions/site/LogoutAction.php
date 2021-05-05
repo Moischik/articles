46c8969435f7da18dbf34e42ece0e62a28c118a7
Moischik <mike.rorikstead@gmail.com>
2021-05-05 12:34:37 +0300

    Create LogoutAction class for site

---
+++ actions/site/LogoutAction.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace app\actions\site;
+
+
+use Yii;
+use yii\base\Action;
+
+class LogoutAction extends Action
+{
+    public function run()
+    {
+        Yii::$app->user->logout();
+
+        return $this->controller->goHome();
+    }
+
+}
